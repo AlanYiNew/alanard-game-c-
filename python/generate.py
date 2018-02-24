2f9e2ca73c53c035211312437f166d194a73d6b9
Alan <245655664@qq.com>
2018-02-24 03:38:07 -0800

    Implementing parser

--- python/generate.py
+++ python/generate.py
@@ -2,9 +2,11 @@
 # -*- coding: UTF-8 -*-
  
 import xml.sax
- 
+
+
 class ProtoHandler( xml.sax.ContentHandler ):
-   def __init__(self):
+    
+   def __init__(self, parser):
       self.CurrentData = ""
       self.type = ""
       self.format = ""
@@ -12,17 +14,31 @@ class ProtoHandler( xml.sax.ContentHandler ):
       self.rating = ""
       self.stars = ""
       self.description = ""
+      self.stack = []
+      self.parser = parser
  
    
    def startElement(self, tag, attributes):
       self.CurrentData = tag
-      if tag == "entry":
-         print ("*****entry*****")
-         pname = attributes["name"]
-         ptype = attributes["type"]
-         pdesc = attributes["desc"]
-         print ("name:"+ str(pname) + ", type:"+ str(ptype) + ", pdesc:" + str(pdesc)) 
- 
+      self.stack.append(tag)
+
+      try:
+         if tag == "entry":
+            if (self.stack[-2] == "interface"):
+               print ("*****interface*****")
+               pname = attributes["name"]
+               ptype = attributes["type"]
+               pdesc = attributes["desc"]
+               print ("name:"+ str(pname) + ", type:"+ str(ptype) + ", pdesc:" + str(pdesc)) 
+            elif (self.stack[-2] == "macrogroup"):
+               pname = attributes["name"]
+               pvalue = attributes["value"]
+      except Exception as err:
+          print("\033[0;31m%s\033[0m" % ("Error line: " + str(self.parser.getLineNumber()) +", Require key or key is invalid, " + str(err)))
+
+
+   def endElement(self, tag):
+       self.stack.pop();
 
 if ( __name__ == "__main__"):
    
@@ -32,7 +48,7 @@ if ( __name__ == "__main__"):
    parser.setFeature(xml.sax.handler.feature_namespaces, 0)
  
 
-   Handler = ProtoHandler()
+   Handler = ProtoHandler(parser)
    parser.setContentHandler( Handler )
    
    parser.parse("protoconfig.xml")
