57e72b9708ee3010ec5f033247053c895426717f
Alan <245655664@qq.com>
2018-02-24 08:47:52 -0800

    Perlinminary parser finised. Next step is to use jinjia to generate protobuf file

--- python/generate.py
+++ python/generate.py
@@ -14,10 +14,13 @@ class ProtoHandler( xml.sax.ContentHandler ):
       self.rating = ""
       self.stars = ""
       self.description = ""
-      self.stack = []
+      self.stack = [] #tag stack
+      self.idst = [] #interface id stack
       self.parser = parser
- 
-   
+      self.interfaces = {}
+      self.macro = {}
+  
+   #==========HANDLER events===========#
    def startElement(self, tag, attributes):
       self.CurrentData = tag
       self.stack.append(tag)
@@ -25,23 +28,48 @@ class ProtoHandler( xml.sax.ContentHandler ):
       try:
          if tag == "entry":
             if (self.stack[-2] == "interface"):
-               print ("*****interface*****")
+               #print ("*****interface*****")
                pname = attributes["name"]
                ptype = attributes["type"]
                pdesc = attributes["desc"]
-               print ("name:"+ str(pname) + ", type:"+ str(ptype) + ", pdesc:" + str(pdesc)) 
+               #print ("name:"+ str(pname) + ", type:"+ str(ptype) + ", pdesc:" + str(pdesc)) 
+               self.interfaces[self.idst[-1]][pname] = {};
+               self.interfaces[self.idst[-1]][pname]["type"] = ptype; 
+               self.interfaces[self.idst[-1]][pname]["desc"] = pdesc;
             elif (self.stack[-2] == "macrogroup"):
                pname = attributes["name"]
                pvalue = attributes["value"]
+               self.macro[pname] = pvalue
+         elif (tag == "interface"):
+            self.idst.append(attributes["id"])
+            self.interfaces[attributes["id"]] = {}
       except Exception as err:
           print("\033[0;31m%s\033[0m" % ("Error line: " + str(self.parser.getLineNumber()) +", Require key or key is invalid, " + str(err)))
           try:
              sys.exit(-1)
           finally:
-             print("Error Exit with error code -1")
+             print("Error Exit with error code (-1)")
 
    def endElement(self, tag):
        self.stack.pop();
+
+   
+   #========DEBUGING Helpers===========#
+   def printDictionary(self, dictionary, prefix):       
+       for i in dictionary:
+         #This will never be true but just implement it using recursive in case
+         if (type(dictionary[i]) == type({})):
+            print(prefix + "name:"+i)
+            self.printDictionary(dictionary[i], prefix+"   ")
+         else:
+            print (prefix+i+":"+str(dictionary[i]))
+
+   def printInterface(self):
+       for i in self.interfaces:
+          print(i+":")
+          self.printDictionary(self.interfaces[i], "   ")     
+
+
 
 if ( __name__ == "__main__"):
    
@@ -55,3 +83,5 @@ if ( __name__ == "__main__"):
    parser.setContentHandler( Handler )
    
    parser.parse("protoconfig.xml")
+
+   Handler.printInterface()
